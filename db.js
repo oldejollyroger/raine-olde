3a125759983a25b9f7098a7c69411579662a20a9
oldejollyroger <rsaladie31@gmail.com>
2026-07-04 21:11:44 +0200

    cb4

--- db.js
+++ db.js
@@ -1,7 +1,5 @@
-// db.js - Conexión a Supabase
-// Sustituye estos valores por los de tu proyecto en Supabase
-const SUPABASE_URL = 'https://maisiydiwreyyuyigwot.supabase.co/rest/v1/';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNpeWRpd3JleXl1eWlnd290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODkwNDcsImV4cCI6MjA5NjA2NTA0N30.0ALGgrqrWK7M1qGOvirs7XKavyu4iqr_xYrHkdikPAQ';
+// db.js CORRECTO
+const SUPABASE_URL = 'https://maisiydiwreyyuyigwot.supabase.co/rest/v1'; // Sin barra al final y sin /rest/v1
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNpeWRpd3JleXl1eWlnd290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODkwNDcsImV4cCI6MjA5NjA2NTA0N30.0ALGgrqrWK7M1qGOvirs7XKavyu4iqr_xYrHkdikPAQ...'; // Tu clave anónima gigante
 
-// Inicializamos el cliente de Supabase
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
