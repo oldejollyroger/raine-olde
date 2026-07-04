a9b80eb2a3d3af953f460b10fc0f9ecdef23f4e4
oldejollyroger <rsaladie31@gmail.com>
2026-07-04 21:15:27 +0200

    cb4.5

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 // db.js CORRECTO
-const SUPABASE_URL = 'https://maisiydiwreyyuyigwot.supabase.co/rest/v1'; // Sin barra al final y sin /rest/v1
+const SUPABASE_URL = 'https://maisiydiwreyyuyigwot.supabase.co'; // Sin barra al final y sin /rest/v1
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1haXNpeWRpd3JleXl1eWlnd290Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA0ODkwNDcsImV4cCI6MjA5NjA2NTA0N30.0ALGgrqrWK7M1qGOvirs7XKavyu4iqr_xYrHkdikPAQ...'; // Tu clave anónima gigante
 
 window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
